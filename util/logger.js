e2ab56c26f9c5b87263bd4bb6f2a31f8a6843ac1
Joe K <joe@dailyraisin.com>
2015-12-31 13:12:30 -0500

    logging prefix

--- util/logger.js
+++ util/logger.js
@@ -2,10 +2,10 @@
 var chalk = require('chalk');
 
 exports.logError = function (errorMessage) {
-  console.log('[npm-cache] ' + chalk.red('[ERROR] ') + errorMessage);
+  console.log('[pkgcache] ' + chalk.red('[ERROR] ') + errorMessage);
 };
 
 exports.logInfo = function (message) {
-  console.log('[npm-cache] ' + chalk.cyan('[INFO] ') + message);
+  console.log('[pkgcache] ' + chalk.cyan('[INFO] ') + message);
 };
 
